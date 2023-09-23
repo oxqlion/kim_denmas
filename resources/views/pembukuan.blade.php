e452e52f5f5b4bcaa420aa7c69143cd6b3f98e74
oxqlion <rafinaya1127@gmail.com>
2023-09-24 00:42:20 +0700

    asdawsd

--- resources/views/pembukuan.blade.php
+++ resources/views/pembukuan.blade.php
@@ -5,13 +5,13 @@
     <div class="container">
         <div class="row">
             <div class="col m-5">
-                <table class="table table-hover-bordered">
-                    @foreach ($pembukuanWithUMKM as $umkm)
-                        <div class="list-group">
-                            <a href="{{ route('pembukuan-admin', ['id' => $umkm->id]) }}">{{ $umkm->umkm_name }}</a>
-                        </div>
+                <div class="list-group">
+                    @foreach ($listUmkm as $umkm)
+                        <a href="#"
+                            class="list-group-item list-group-item-action {{ $umkm->nama == 'dummy1' ? 'active' : '' }}">{{ $umkm->nama }}</a>
                     @endforeach
-                </table>
+
+                </div>
             </div>
             <div class="col">
                 <div class="overflow">
@@ -23,7 +23,7 @@
                             <td>Harga</td>
                             <td>Total</td>
                         </tr>
-                        @foreach ($pembukuanWithUMKM as $buku)
+                        @foreach ($pembukuankWithUMKM as $buku)
                             <tr>
                                 <td>{{ $buku->nama }}</td>
                                 <td>{{ $buku->jumlah }}</td>
--- resources/views/stok.blade.php
+++ resources/views/stok.blade.php
@@ -1,42 +1,43 @@
 @extends('components.parent')
 
 @section('content')
-<div>
+    <div>
 
-    <div class="container">
-        <div class="row">
-            <div class="col m-5">
-                <div class="list-group">
-                    @foreach ($listUmkm as $umkm)
-                    <a href="#" class="list-group-item list-group-item-action {{ ($umkm->nama == "dummy1")? "active":"" }}">{{ $umkm->nama }}</a>
-                    @endforeach
-                    
-                  </div>
-            </div>
-    
-            <div class="col">
+        <div class="container">
+            <div class="row">
+                <div class="col m-5">
+                    <div class="list-group">
+                        @foreach ($listUmkm as $umkm)
+                            <a href="#"
+                                class="list-group-item list-group-item-action {{ $umkm->nama == 'dummy1' ? 'active' : '' }}">{{ $umkm->nama }}</a>
+                        @endforeach
 
-        <table class="table table-bordered">
-            <thead>
-                <tr>
-                    <th>Name</th>
-                    <th>Type</th>
-                    <th>Amount</th>
-                </tr>
-            </thead>
-            <tbody>
-                @foreach ($stokUmkm as $umkm)
-                <tr>
-                    <td>{{ $umkm->nama }}</td>
-                    <td>{{ $umkm->tipe }}</td>
-                    <td>{{ $umkm->jumlah }}</td>
-                </tr>
-                @endforeach
-            </tbody>
-        </table>
-    
+                    </div>
+                </div>
+
+                <div class="col">
+
+                    <table class="table table-bordered">
+                        <thead>
+                            <tr>
+                                <th>Name</th>
+                                <th>Type</th>
+                                <th>Amount</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @foreach ($stokUmkm as $umkm)
+                                <tr>
+                                    <td>{{ $umkm->nama }}</td>
+                                    <td>{{ $umkm->tipe }}</td>
+                                    <td>{{ $umkm->jumlah }}</td>
+                                </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
+
+                </div>
             </div>
         </div>
     </div>
-</div>
 @endsection
