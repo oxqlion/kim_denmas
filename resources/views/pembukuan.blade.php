83ac292e5a276ae824ee27c116337b2d1413c001
oxqlion <rafinaya1127@gmail.com>
2023-09-24 01:05:14 +0700

    adjhiwuha

--- resources/views/pembukuan.blade.php
+++ resources/views/pembukuan.blade.php
@@ -2,36 +2,38 @@
 
 
 @section('content')
-    <div class="container">
-        <div class="row">
-            <div class="col m-5">
-                <div class="list-group">
-                    @foreach ($listUmkm as $umkm)
-                        <a href="#"
-                            class="list-group-item list-group-item-action {{ $umkm->nama == 'dummy1' ? 'active' : '' }}">{{ $umkm->nama }}</a>
-                    @endforeach
+    <div>
 
-                </div>
-            </div>
-            <div class="col">
-                <div class="overflow">
-
-                    <table class="table table-hover-bordered">
-                        <tr>
-                            <td>Nama</td>
-                            <td>Kuantitas</td>
-                            <td>Harga</td>
-                            <td>Total</td>
-                        </tr>
-                        @foreach ($pembukuankWithUMKM as $buku)
-                            <tr>
-                                <td>{{ $buku->nama }}</td>
-                                <td>{{ $buku->jumlah }}</td>
-                                <td>{{ $buku->harga }}</td>
-                                <td>{{ $buku->total }}</td>
-                            </tr>
+        <div class="container">
+            <div class="row">
+                <div class="col m-4">
+                    <div class="list-group">
+                        @foreach ($listUmkm as $umkm)
+                            <a href="#"
+                                class="list-group-item list-group-item-action {{ $umkm->nama == 'dummy1' ? 'active' : '' }}">{{ $umkm->nama }}</a>
                         @endforeach
-                    </table>
+
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="overflow">
+                        <table class="table table-bordered">
+                            <tr>
+                                <th>Nama</th>
+                                <th>Kuantitas</th>
+                                <th>Harga</th>
+                                <th>Total</th>
+                            </tr>
+                            @foreach ($pembukuankWithUMKM as $buku)
+                                <tr>
+                                    <td>{{ $buku->nama }}</td>
+                                    <td>{{ $buku->jumlah }}</td>
+                                    <td>{{ $buku->harga }}</td>
+                                    <td>{{ $buku->total }}</td>
+                                </tr>
+                            @endforeach
+                        </table>
+                    </div>
                 </div>
             </div>
         </div>
