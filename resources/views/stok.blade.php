d55f101d132cc32acfa5a93e7572da97834a22c9
hayyaoe <hayya947@gmail.com>
2023-09-23 22:19:52 +0700

    nice

--- resources/views/stok.blade.php
+++ resources/views/stok.blade.php
@@ -5,5 +5,22 @@
     @foreach ($stokWithUMKM as $umkm)
         <h1>{{ $umkm->umkm_name }}</h1>
     @endforeach
+
+    <div class="container">
+        <div class="row">
+            <div class="col m-5">
+                <div class="list-group">
+                    @foreach ($stokWithUMKM as $umkm)
+                    <a href="#" class="list-group-item list-group-item-action {{ ($umkm->nama == "dummy1")? "active":"" }}">{{ $umkm->nama }}</a>
+                    @endforeach
+                    
+                  </div>
+            </div>
+    
+            <div class="col">
+    
+            </div>
+        </div>
+    </div>
 </div>
 @endsection
